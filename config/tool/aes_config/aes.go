768f36fbc15653939ad5fad617d2ef5825790df3
jinlongchen <jinlongchen@live.com>
2019-07-31 01:09:18 +0800

    f

---
+++ config/tool/aes_config/aes.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"github.com/jinlongchen/golang-utilities/config"
+	"github.com/jinlongchen/golang-utilities/crypto"
+	"github.com/jinlongchen/golang-utilities/log"
+)
+
+func main() {
+	aesKey := flag.String("aeskey", "", "")
+	aesKeySalt := flag.String("aeskeysalt", "", "")
+	typ := flag.String("type", "", "enc or dec")
+	str := flag.String("str", "", "")
+
+	flag.Parse()
+
+	println("aeskey:", *aesKey)
+	println("aeskeysalt:", *aesKeySalt)
+	println("type:", *typ)
+	println("str:", *typ)
+
+	cfg := &config.Config{
+		AesKey: crypto.String(*aesKey + *aesKeySalt).GetMd5(),
+	}
+	if *typ == "enc" {
+		encrypted := cfg.EncryptString(*str)
+		log.Infof("encrypted: %s", encrypted)
+	} else {
+		decrypted := cfg.DecryptString(*str)
+		log.Infof("decrypted: %s", decrypted)
+	}
+}
