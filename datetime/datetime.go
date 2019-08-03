4b98217c4903bc83fc0fe510a2d19026542db485
jinlongchen <jinlongchen@live.com>
2019-08-03 23:09:50 +0800

    ff

--- datetime/datetime.go
+++ datetime/datetime.go
@@ -10,3 +10,8 @@ func Date(t time.Time) time.Time {
 	y, m, d := t.Date()
 	return time.Date(y, m, d, 0, 0, 0, 0, t.Location())
 }
+
+func Today() time.Time {
+	t := time.Now()
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
+}
