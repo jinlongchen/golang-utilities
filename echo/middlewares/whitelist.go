ffb5d3c0d4ed07ebf8c68fb90072c927d327d4dc
jinlongchen <jinlongchen@live.com>
2019-08-03 22:49:22 +0800

    wl

---
+++ echo/middlewares/whitelist.go
@@ -0,0 +1,36 @@
+package middlewares
+
+
+import (
+	"fmt"
+	"github.com/jinlongchen/golang-utilities/log"
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"regexp"
+	"strings"
+)
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+func WhiteList(getList func() []string) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			list := getList()
+			if len(list) == 0 {
+				return next(c)
+			}
+			log.Debugf("remote ip:%s", c.Request().Header.Get("X-Real-IP"))
+			for _, ipPattern := range list {
+				ipPattern = strings.Replace(ipPattern, "*", `\d{1,3}`, -1)
+				ipPattern = strings.Replace(ipPattern, ".", `\.`, -1)
+				ipPattern = fmt.Sprintf(`^%s$`, ipPattern)
+				log.Debugf("ipPattern:%s", ipPattern)
+				matched, err := regexp.MatchString(ipPattern, c.Request().Header.Get("X-Real-IP"))
+				if err == nil && matched {
+					return next(c)
+				}
+			}
+			return c.NoContent(http.StatusUnauthorized)
+		}
+	}
+}
