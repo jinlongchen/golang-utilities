6912ea333e142f622b1ee7d23e4f2b1e7ca30319
jinlongchen <jinlongchen@live.com>
2019-08-06 23:21:47 +0800

    ff

--- json/json.go
+++ json/json.go
@@ -14,6 +14,14 @@ func Marshal(v interface{}) ([]byte, error) {
 	return jsoniter.ConfigCompatibleWithStandardLibrary.Marshal(v)
 }
 
+func ShouldMarshal(v interface{}) []byte {
+	ret ,err := jsoniter.ConfigCompatibleWithStandardLibrary.Marshal(v)
+	if err != nil {
+		return nil
+	}
+	return ret
+}
+
 func MarshalIndent(v interface{}, prefix, indent string) ([]byte, error) {
 	return jsoniter.ConfigCompatibleWithStandardLibrary.MarshalIndent(v,prefix,indent)
 }
