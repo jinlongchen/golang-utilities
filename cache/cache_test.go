ee3dc31ee5c72f3c2462f87ba3ccade785978a47
jinlongchen <jinlongchen@live.com>
2019-08-13 17:33:12 +0800

    ff

---
+++ cache/cache_test.go
@@ -0,0 +1,38 @@
+package cache
+
+import (
+	"log"
+	"testing"
+	"time"
+)
+
+func TestRedisCache_Get(t *testing.T) {
+	var c Cache
+	c = NewRedisCache(
+		map[string]string{
+			"c1": "127.0.0.1:6379",
+		},
+		"",
+	)
+	defer c.Close()
+	var g string
+	err := c.Get("test_cache", &g)
+	if err != nil {
+		log.Fatalln("get err:", err.Error())
+	}
+	log.Println("val:", g)
+}
+func TestRedisCache_Set(t *testing.T) {
+	var c Cache
+	c = NewRedisCache(
+		map[string]string{
+			"c1": "127.0.0.1:6379",
+		},
+		"",
+	)
+	defer c.Close()
+	err := c.Set("test_cache", time.Now().String(), time.Hour)
+	if err != nil {
+		log.Fatalln("set err:", err.Error())
+	}
+}
