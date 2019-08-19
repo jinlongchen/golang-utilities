963110b282c2a972d8d8378b5877a66b8455f0d8
jinlongchen <jinlongchen@live.com>
2019-08-19 23:29:13 +0800

    ff

---
+++ amap/string.go
@@ -0,0 +1,41 @@
+package amap
+
+import (
+	"bytes"
+	"errors"
+)
+
+var (
+	sbArray = []byte(`[]`)
+)
+type String []byte
+
+// MarshalJSON returns m as the JSON encoding of m.
+func (m String) MarshalJSON() ([]byte, error) {
+	if m == nil {
+		return []byte(""), nil
+	}
+	if bytes.Compare(sbArray, m) == 0 {
+		return []byte(""), nil
+	}
+	return m, nil
+}
+
+// UnmarshalJSON sets *m to a copy of data.
+func (m *String) UnmarshalJSON(data []byte) error {
+	if m == nil {
+		return errors.New("amap.String: UnmarshalJSON on nil pointer")
+	}
+	if bytes.Compare(sbArray, data) == 0 {
+		return nil
+	}
+	*m = append((*m)[0:0], data...)
+	return nil
+}
+
+func (m String) ToString() string {
+	if m == nil {
+		return ""
+	}
+	return string(m)
+}
