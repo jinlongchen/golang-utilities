a3b0b82558d204816f810af260af3691170d97ec
jinlongchen <jinlongchen@live.com>
2019-08-21 13:37:26 +0800

    ff

---
+++ datetime/utc_time.go
@@ -0,0 +1,62 @@
+package datetime
+
+import (
+	"github.com/araddon/dateparse"
+	"github.com/jinlongchen/golang-utilities/json"
+	"strings"
+	"time"
+)
+
+type UTCTime time.Time
+
+func (dt UTCTime) MarshalJSON() ([]byte, error) {
+	tmp := time.Time(dt)
+	return json.ShouldMarshal(tmp.UTC().Format(time.RFC3339)), nil
+}
+
+func (dt *UTCTime) UnmarshalJSON(p []byte) error {
+	t, err := dateparse.ParseAny(strings.Replace(
+		string(p),
+		"\"",
+		"",
+		-1,
+	))
+	if err != nil {
+		t, err = time.Parse(time.RFC3339, strings.Replace(
+			string(p),
+			"\"",
+			"",
+			-1,
+		))
+		if err != nil {
+			t, err = time.Parse("2006-01-02T15:04Z07:00", strings.Replace(
+				string(p),
+				"\"",
+				"",
+				-1,
+			))
+		}
+	}
+	if err != nil {
+		return err
+	}
+	*dt = UTCTime(t)
+
+	return nil
+}
+
+func (dt *UTCTime) BeforeEpoch() bool {
+	if dt == nil {
+		return false
+	}
+	t := time.Time(*dt)
+	return t.Before(time.Unix(0, 0))
+}
+
+func (dt *UTCTime) AfterEpoch() bool {
+	if dt == nil {
+		return false
+	}
+	t := time.Time(*dt)
+	return t.After(time.Unix(0, 0))
+}
