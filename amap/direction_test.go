b6c0024a9bddbea8b02e273994e027f371d539dc
Jinlong Chen <jinlongchen@live.com>
2019-10-22 02:59:39 +0800

    time duration

--- amap/direction_test.go
+++ amap/direction_test.go
@@ -11,7 +11,6 @@ import (
 
 func TestDistance(t *testing.T) {
 	amapKey := ""
-	//http://restapi.amap.com/v3/distance?origins=104.073830,30.494952&destination=104.065810,30.657468&output=json&key=8568cc10c01a137923e2885a297dbf9f&type=1
 	distance, duration, err := Distance(
 		104.073830,
 		30.494952,
