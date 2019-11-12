d4d199d00bed1b26648e9cdfb1eed8d20953be5f
Jinlong Chen <jinlongchen@live.com>
2019-11-12 21:47:49 +0800

    ff

--- datetime/datetime_test.go
+++ datetime/datetime_test.go
@@ -1,1 +1,14 @@
 package datetime
+
+import (
+	"github.com/jinlongchen/golang-utilities/log"
+	"testing"
+)
+
+func TestParseWithTimeZone(t *testing.T) {
+	ti, err := ParseWithTimeZone("2006-01-02 15:04", "2019-11-12 20:13:14", TimeZoneShanghai)
+	if err != nil {
+		log.Errorf("parse err: %s", err.Error())
+	}
+	log.Infof("time: %s", ti.String())
+}
