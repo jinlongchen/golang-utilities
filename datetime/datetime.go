2da2ddf8b293d57e82baf21a30f22f374466b53e
Jinlong Chen <jinlongchen@live.com>
2019-11-12 21:47:49 +0800

    ff

--- datetime/datetime.go
+++ datetime/datetime.go
@@ -1,1 +1,27 @@
 package datetime
+
+import (
+	"github.com/araddon/dateparse"
+	"time"
+)
+
+type TimeZone string
+
+const (
+	TimeZoneShanghai TimeZone = "Asia/Shanghai"
+)
+
+func ParseWithTimeZone(layout string, value string, timezone TimeZone) (time.Time, error) {
+	l, err := time.LoadLocation(string(timezone))
+	if err != nil {
+		l = time.Local
+	}
+	if layout != "" {
+		lt, err := time.ParseInLocation(layout, value, l)
+		if err == nil {
+			return lt, nil
+		}
+	}
+	ret, err := dateparse.ParseIn(value, l)
+	return ret, err
+}
