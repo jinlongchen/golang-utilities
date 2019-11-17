6aa2806a93dbda007c982590e582d90e49c5cfb8
Jinlong Chen <jinlongchen@live.com>
2019-11-17 11:33:24 +0800

    ff

---
+++ json/json_test.go
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2019. Qing Cheng Technology Co., Ltd.
+ */
+
+package json
+
+import (
+	"fmt"
+	jsoniter "github.com/json-iterator/go"
+	"testing"
+)
+
+type message struct {
+	Code    string              `json:"code,omitempty"`
+	Message string              `json:"message,omitempty"`
+	Data    jsoniter.RawMessage `json:"data,omitempty"`
+}
+
+func TestShouldMarshal(t *testing.T) {
+	fmt.Println(string(ShouldMarshal(&message{
+		Code: "123",
+		Data: ShouldMarshal("456"),
+	})))
+}
