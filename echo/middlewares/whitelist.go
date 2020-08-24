7f47e4016d1c86e48360cf5bf704d716c7bc1dae
Jinlong Chen <jinlongchen@live.com>
2020-08-24 16:21:43 +0800

    log

--- echo/middlewares/whitelist.go
+++ echo/middlewares/whitelist.go
@@ -18,12 +18,12 @@ func WhiteList(getList func() []string) echo.MiddlewareFunc {
 			if len(list) == 0 {
 				return next(c)
 			}
-			log.Debugf("remote ip:%s", c.Request().Header.Get("X-Real-IP"))
+			log.Debugf(nil, "remote ip:%s", c.Request().Header.Get("X-Real-IP"))
 			for _, ipPattern := range list {
 				ipPattern = strings.Replace(ipPattern, "*", `\d{1,3}`, -1)
 				ipPattern = strings.Replace(ipPattern, ".", `\.`, -1)
 				ipPattern = fmt.Sprintf(`^%s$`, ipPattern)
-				log.Debugf("ipPattern:%s", ipPattern)
+				log.Debugf(nil, "ipPattern:%s", ipPattern)
 				matched, err := regexp.MatchString(ipPattern, c.Request().Header.Get("X-Real-IP"))
 				if err == nil && matched {
 					return next(c)
