accd18001f3d806e54d67a6d94a91ed440b1f32e
jinlong chen <jinlongchen@live.com>
2021-01-11 12:27:07 +0800

    baidu audit

---
+++ baidu/baidu.go
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2020. Jinlong Chen.
+ */
+
+package baidu
+
+import (
+	"fmt"
+	"github.com/jinlongchen/golang-utilities/cache"
+	"github.com/jinlongchen/golang-utilities/config"
+	"github.com/jinlongchen/golang-utilities/log"
+	"sync"
+)
+
+type Baidu struct {
+	cache   cache.Cache
+	memory  *sync.Map
+	config  *config.Config
+	logFunc func(string)
+	quit    chan struct{}
+}
+
+func NewBaidu(cah cache.Cache, config *config.Config, logFunc func(string)) *Baidu {
+	ret := &Baidu{
+		cache:   cah,
+		memory:  new(sync.Map),
+		config:  config,
+		logFunc: logFunc,
+		quit:    make(chan struct{}),
+	}
+
+	return ret
+}
+
+func (bd *Baidu) Exit() error {
+	close(bd.quit)
+	return nil
+}
+
+func (bd *Baidu) logf(format string, args ...interface{}) {
+	if bd.logFunc != nil {
+		bd.logFunc(fmt.Sprintf(format, args...))
+	}
+	log.Infof(format, args...)
+}
