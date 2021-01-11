c0bb969aa80a919e66df54a832e3f47f86304d72
jinlong chen <jinlongchen@live.com>
2021-01-11 12:27:07 +0800

    baidu audit

---
+++ baidu/audit_text.go
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2018. Jinlong Chen.
+ */
+
+package baidu
+
+import (
+	"encoding/json"
+	"github.com/jinlongchen/golang-utilities/http"
+	"net/url"
+)
+
+func (bd *Baidu) AuditText(str string, appId, appSecret string) (*BaiduAuditResult, error) {
+	accessToken, err := bd.GetAccessTokenBceByClient(appId, appSecret)
+	if err != nil {
+		bd.logf("cannot get access token(%v): %v", appId, err.Error())
+		return nil, err
+	}
+
+	detectURL, _ := url.Parse(`https://aip.baidubce.com/rest/2.0/solution/v1/text_censor/v2/user_defined`)
+	detectQuery := detectURL.Query()
+	detectQuery.Set("access_token", accessToken.AccessToken)
+	detectURL.RawQuery = detectQuery.Encode()
+
+	params := url.Values{
+		"text": {str},
+	}
+	bdRecognizeResultData, err := http.PostData(detectURL.String(), "application/x-www-form-urlencoded", []byte(params.Encode()))
+
+	if err != nil {
+		bd.logf("recognize picture err: %v", err)
+		return nil, err
+	}
+	baiduAuditTextResult := &BaiduAuditResult{}
+	err = json.Unmarshal(bdRecognizeResultData, baiduAuditTextResult)
+	if err != nil {
+		bd.logf("recognize picture err: %v", err)
+		return nil, err
+	}
+	return baiduAuditTextResult, nil
+}
