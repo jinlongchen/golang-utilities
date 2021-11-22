9898733f6d9813cec7cd0a5e93bd8bafd55cbf68
jinlong chen <jinlongchen@live.com>
2021-11-22 08:57:26 +0800

    banner write

--- banner/banner.go
+++ banner/banner.go
@@ -1,11 +1,41 @@
 package banner
 
 import (
-	"github.com/arsham/figurine/figurine"
+	"bytes"
+	"fmt"
+	"github.com/jinlongchen/golang-utilities/banner/figure"
 	_ "github.com/jinlongchen/golang-utilities/banner/statik"
+	"io"
+	"math/rand"
 	"os"
+
+	"github.com/arsham/rainbow/rainbow"
+	"github.com/pkg/errors"
+	"github.com/rakyll/statik/fs"
 )
 
 func Print(s string) {
-	_ = figurine.Write(os.Stdout, s, "Calvin S.flf")
+	_ = Write(os.Stdout, s, "Calvin S.flf")
+}
+
+func Write(out io.Writer, msg, fontName string) error {
+	fileS, err := fs.New()
+	if err != nil {
+		return err
+	}
+	font, err := fileS.Open(fmt.Sprintf("/%s", fontName))
+	if err != nil {
+		return errors.Wrap(err, fontName)
+	}
+	buf := &bytes.Buffer{}
+	myFigure := figure.NewFigureWithFont(msg, font, true)
+	figure.Write(buf, myFigure)
+	l := &rainbow.Light{
+		Writer: out,
+		Seed:   rand.Int63n(256),
+	}
+	if _, err := io.Copy(l, buf); err != nil {
+		return err
+	}
+	return nil
 }
