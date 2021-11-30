da24e7ce024a849fc5bc07e686c550e87ca0b3c3
jinlong chen <jinlongchen@live.com>
2021-11-30 11:44:31 +0800

    remote ip u

--- echo/middlewares/whitelist.go
+++ echo/middlewares/whitelist.go
@@ -1,12 +1,10 @@
 package middlewares
 
 import (
-	"fmt"
 	"github.com/jinlongchen/golang-utilities/log"
 	"github.com/labstack/echo/v4"
+	"net"
 	"net/http"
-	"regexp"
-	"strings"
 )
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -18,18 +16,42 @@ func WhiteList(getList func() []string) echo.MiddlewareFunc {
 			if len(list) == 0 {
 				return next(c)
 			}
-			log.Debugf("remote ip:%s", c.Request().Header.Get("X-Real-IP"))
-			for _, ipPattern := range list {
-				ipPattern = strings.Replace(ipPattern, "*", `\d{1,3}`, -1)
-				ipPattern = strings.Replace(ipPattern, ".", `\.`, -1)
-				ipPattern = fmt.Sprintf(`^%s$`, ipPattern)
-				log.Debugf("ipPattern:%s", ipPattern)
-				matched, err := regexp.MatchString(ipPattern, c.Request().Header.Get("X-Real-IP"))
-				if err == nil && matched {
+			remoteIP := getRemoteIP(c.Request())
+			if remoteIP == "" {
+				return c.NoContent(http.StatusUnauthorized)
+			}
+
+			log.Debugf("remote ip:%s", remoteIP)
+			ipAddr := net.ParseIP(remoteIP)
+
+			for _, networkAddr := range list {
+				_, ipNetworkAddr, err := net.ParseCIDR(networkAddr)
+				if err != nil {
+					return c.NoContent(http.StatusUnauthorized)
+				}
+				if ipNetworkAddr.Contains(ipAddr) {
+					log.Debugf("%s contain %s?\n", ipNetworkAddr, ipAddr)
 					return next(c)
+				} else {
+					log.Debugf("%s not contain %s?\n", ipNetworkAddr, ipAddr)
 				}
 			}
 			return c.NoContent(http.StatusUnauthorized)
 		}
 	}
+}
+func getRemoteIP(req *http.Request) string {
+	if req.Header.Get("X-Forwarded-For") != "" {
+		return req.Header.Get("X-Forwarded-For")
+	}
+	if req.Header.Get("X-Real-IP") != "" {
+		return req.Header.Get("X-Real-IP")
+	}
+	if req.Header.Get("Proxy-Client-IP") != "" {
+		return req.Header.Get("Proxy-Client-IP")
+	}
+	if req.Header.Get("WL-Proxy-Client-IP") != "" {
+		return req.Header.Get("WL-Proxy-Client-IP")
+	}
+	return ""
 }
