a9656da8c0de046f746581b6e61c0509d4f9c3d7
JinlongChen <jinlongchen@live.com>
2022-03-16 16:51:32 +0800

    json

--- json/json_test.go
+++ json/json_test.go
@@ -24,6 +24,27 @@ func TestShouldMarshal(t *testing.T) {
 }
 
 func TestShouldMarshal2(t *testing.T) {
-	fmt.Println(string(ShouldMarshal(&map[string]interface{}{
-	})))
+	fmt.Println(string(ShouldMarshal(&map[string]interface{}{})))
+}
+
+type ImageUploadRequest struct {
+	ID         string `json:"id"`
+	Type       string `json:"type"`
+	Order      int    `json:"order"`
+	Width      int    `json:"width"`
+	Height     int    `json:"height"`
+	MutedColor string `json:"muted_color"`
+	Cover      bool   `json:"cover"`
+}
+
+// go test -c -o json.test -gcflags all=-N -l github.com/jinlongchen/golang-utilities/json #gosetup
+func TestUnmarshalV2(t *testing.T) {
+	fmt.Printf("%v\n", "TestUnmarshalV2")
+	if info, err := UnmarshalV2[ImageUploadRequest]([]byte(`{"type":"image","order":null,"width":720,"height":1280,"muted_color":null,"cover":null}`)); err == nil {
+		fmt.Printf("%v\n", info)
+		fmt.Printf("%v\n", info.Order)
+		fmt.Printf("%v\n", string(ShouldMarshal(info)))
+	} else {
+		fmt.Printf("unmarshal err: %v\n", err)
+	}
 }
