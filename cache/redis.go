8986e7c83698db7791f27b002a52283d3ae1b0f9
JinlongChen <jinlongchen@live.com>
2022-12-07 00:29:13 +0800

    cache v8

--- cache/redis.go
+++ cache/redis.go
@@ -1,19 +1,17 @@
 package cache
 
 import (
-    "encoding/hex"
+    "context"
     "time"
 
-    redisCache "github.com/go-redis/cache"
-    "github.com/go-redis/redis"
-
-    "github.com/jinlongchen/golang-utilities/json"
-    "github.com/jinlongchen/golang-utilities/log"
+    redisCache "github.com/go-redis/cache/v8"
+    "github.com/go-redis/redis/v8"
+    // redisCache "github.com/jinlongchen/redis-cache-go"
 )
 
 type RedisCache struct {
-    codec *redisCache.Codec
-    ring  *redis.Ring
+    rCache *redisCache.Cache
+    ring   *redis.Ring
 }
 
 func NewRedisCache(addrs map[string]string, pwd string) Cache {
@@ -22,34 +20,44 @@ func NewRedisCache(addrs map[string]string, pwd string) Cache {
         Password: pwd,
     })
 
-    codec := &redisCache.Codec{
-        Redis: ring,
-        Marshal: func(v interface{}) ([]byte, error) {
-            return json.Marshal(v)
-        },
-        Unmarshal: func(b []byte, v interface{}) error {
-            err := json.Unmarshal(b, v)
-            if err != nil {
-                log.Errorf(
-                    `cannot unmarshal data: %v, %v`,
-                    err,
-                    hex.EncodeToString(b))
-            }
-            return err
-        },
-    }
+    // codec := &redisCache.Codec{
+    //     Redis: ring,
+    //     Marshal: func(v interface{}) ([]byte, error) {
+    //         return json.Marshal(v)
+    //     },
+    //     Unmarshal: func(b []byte, v interface{}) error {
+    //         err := json.Unmarshal(b, v)
+    //         if err != nil {
+    //             log.Errorf(
+    //                 `cannot unmarshal data: %v, %v`,
+    //                 err,
+    //                 hex.EncodeToString(b))
+    //         }
+    //         return err
+    //     },
+    // }
+    // return &RedisCache{
+    //     codec: codec,
+    //     ring:  ring,
+    // }
+
+    rCache := redisCache.New(&redisCache.Options{
+        Redis:      ring,
+        LocalCache: redisCache.NewTinyLFU(1000, time.Minute),
+    })
+
     return &RedisCache{
-        codec: codec,
-        ring:  ring,
+        rCache: rCache,
+        ring:   ring,
     }
 }
 
 func (c *RedisCache) Delete(key string) error {
-    return c.codec.Delete(key)
+    return c.rCache.Delete(context.TODO(), key)
 }
 
 func (c *RedisCache) Get(key string, obj interface{}) error {
-    err := c.codec.Get(key, &obj)
+    err := c.rCache.Get(context.TODO(), key, &obj)
     if err == nil {
         return nil
     }
@@ -57,10 +65,10 @@ func (c *RedisCache) Get(key string, obj interface{}) error {
 }
 
 func (c *RedisCache) Set(key string, obj interface{}, timeout time.Duration) error {
-    return c.codec.Set(&redisCache.Item{
-        Key:        key,
-        Object:     obj,
-        Expiration: timeout,
+    return c.rCache.Set(&redisCache.Item{
+        Key:   key,
+        Value: obj,
+        TTL:   timeout,
     })
 }
 func (c *RedisCache) Close() error {
--- go.mod
+++ go.mod
@@ -7,8 +7,8 @@ require (
 	github.com/araddon/dateparse v0.0.0-20190622164848-0fb0a474d195
 	github.com/arsham/rainbow v1.0.0
 	github.com/fsnotify/fsnotify v1.4.9
-	github.com/go-redis/cache v6.4.0+incompatible
-	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/go-redis/cache/v8 v8.4.4
+	github.com/go-redis/redis/v8 v8.11.3
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/hashicorp/hcl v1.0.0
 	github.com/jmoiron/sqlx v1.2.0
@@ -44,16 +44,19 @@ require (
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 	gopkg.in/ini.v1 v1.66.2
-	gopkg.in/yaml.v2 v2.3.0
+	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
 	github.com/arsham/strings v0.0.0-20181015215346-d1b453ead28d // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
 	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
@@ -66,11 +69,14 @@ require (
 	github.com/opentracing/opentracing-go v1.1.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
-	github.com/vmihailenco/msgpack v3.3.3+incompatible // indirect
+	github.com/vmihailenco/go-tinylfu v0.2.2 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.3.4 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	go.etcd.io/etcd/client/pkg/v3 v3.5.1 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/net v0.2.0 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.2.0 // indirect
 	google.golang.org/genproto v0.0.0-20210602131652-f16073e35f0c // indirect
 	google.golang.org/grpc v1.38.0 // indirect
