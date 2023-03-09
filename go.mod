b95775ef07b491a457336678e7eb9c9d1f099ad9
JinlongChen <jinlongchen@live.com>
2023-03-09 08:05:03 +0800

    go svc

--- go.mod
+++ go.mod
@@ -7,13 +7,11 @@ require (
 	github.com/araddon/dateparse v0.0.0-20190622164848-0fb0a474d195
 	github.com/arsham/rainbow v1.0.0
 	github.com/fsnotify/fsnotify v1.4.9
-	github.com/go-redis/cache/v8 v8.4.4
-	github.com/go-redis/redis/v8 v8.11.3
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/hashicorp/hcl v1.0.0
 	github.com/jmoiron/sqlx v1.2.0
 	github.com/json-iterator/go v1.1.12
-	github.com/judwhite/go-svc v1.1.2
+	github.com/judwhite/go-svc v1.2.1
 	github.com/labstack/echo/v4 v4.9.0
 	github.com/labstack/gommon v0.3.1
 	github.com/lib/pq v1.2.0
@@ -50,37 +48,30 @@ require (
 
 require (
 	github.com/arsham/strings v0.0.0-20181015215346-d1b453ead28d // indirect
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
-	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
-	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
 	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mattn/go-sqlite3 v1.11.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/naoina/go-stringutil v0.1.0 // indirect
-	github.com/onsi/ginkgo v1.16.5 // indirect
-	github.com/onsi/gomega v1.24.1 // indirect
 	github.com/opentracing/opentracing-go v1.1.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
-	github.com/vmihailenco/go-tinylfu v0.2.2 // indirect
-	github.com/vmihailenco/msgpack/v5 v5.3.4 // indirect
-	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	go.etcd.io/etcd/client/pkg/v3 v3.5.1 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/net v0.2.0 // indirect
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
-	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
 	google.golang.org/genproto v0.0.0-20210602131652-f16073e35f0c // indirect
 	google.golang.org/grpc v1.38.0 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- svc/execute.go
+++ svc/execute.go
@@ -1,114 +1,114 @@
 package svc
 
 import (
-    "net/url"
-    "os"
-    "path"
-    "path/filepath"
-    "syscall"
-    "time"
+	"net/url"
+	"os"
+	"path"
+	"path/filepath"
+	"syscall"
+	"time"
 
-    goSvc "github.com/judwhite/go-svc/svc"
+	goSvc "github.com/judwhite/go-svc"
 
-    "github.com/jinlongchen/golang-utilities/config"
-    "github.com/jinlongchen/golang-utilities/log"
+	"github.com/jinlongchen/golang-utilities/config"
+	"github.com/jinlongchen/golang-utilities/log"
 )
 
 type Executor struct {
-    s   Service
-    cfg *config.Config
+	s   Service
+	cfg *config.Config
 }
 
 func Execute(s Service) {
-    e := &Executor{}
-    e.s = s
-    if err := goSvc.Run(e, syscall.SIGINT, syscall.SIGTERM); err != nil {
-        panic(err)
-    }
+	e := &Executor{}
+	e.s = s
+	if err := goSvc.Run(e, syscall.SIGINT, syscall.SIGTERM); err != nil {
+		panic(err)
+	}
 }
 
 func (e *Executor) Init(env goSvc.Environment) error {
-    if env.IsWindowsService() {
-        dir := filepath.Dir(os.Args[0])
-        err := os.Chdir(dir)
-        if err != nil {
-            panic(err.Error())
-        }
-    }
-    remoteConfigURL := os.Getenv("REMOTE_CONFIG_URL")
-    localConfigURL := os.Getenv("LOCAL_CONFIG_URL")
+	if env.IsWindowsService() {
+		dir := filepath.Dir(os.Args[0])
+		err := os.Chdir(dir)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	remoteConfigURL := os.Getenv("REMOTE_CONFIG_URL")
+	localConfigURL := os.Getenv("LOCAL_CONFIG_URL")
 
-    if remoteConfigURL != "" {
-        uRL, err := url.Parse(remoteConfigURL)
-        if err == nil {
-            e.cfg = config.NewRemoteConfig(uRL.Scheme, uRL.Host, uRL.Path, "toml")
-            if e.cfg == nil {
-                retry := 600
-                for i := 0; i < retry; i++ {
-                    log.Infof("wait load config")
-                    time.Sleep(time.Second)
-                    e.cfg = config.NewRemoteConfig(uRL.Scheme, uRL.Host, uRL.Path, "toml")
-                    if e.cfg != nil {
-                        break
-                    }
-                    if i+1 == retry {
-                        log.Fatalf("cannot load config")
-                    }
-                }
-            }
-            if localConfigURL != "" {
-                dir := path.Dir(localConfigURL)
-                _, err := os.Stat(dir)
-                if os.IsNotExist(err) {
-                    _ = os.MkdirAll(dir, 0777)
-                }
-                _ = e.cfg.Save(localConfigURL)
-            }
-        }
-    }
-    if e.cfg == nil {
-        if localConfigURL == "" {
-            if _, err := os.Stat("conf-file.toml"); err == nil {
-                localConfigURL = "conf-file.toml"
-            }
-        }
-        if localConfigURL != "" {
-            e.cfg = config.NewConfig(localConfigURL)
-        }
-    }
+	if remoteConfigURL != "" {
+		uRL, err := url.Parse(remoteConfigURL)
+		if err == nil {
+			e.cfg = config.NewRemoteConfig(uRL.Scheme, uRL.Host, uRL.Path, "toml")
+			if e.cfg == nil {
+				retry := 600
+				for i := 0; i < retry; i++ {
+					log.Infof("wait load config")
+					time.Sleep(time.Second)
+					e.cfg = config.NewRemoteConfig(uRL.Scheme, uRL.Host, uRL.Path, "toml")
+					if e.cfg != nil {
+						break
+					}
+					if i+1 == retry {
+						log.Fatalf("cannot load config")
+					}
+				}
+			}
+			if localConfigURL != "" {
+				dir := path.Dir(localConfigURL)
+				_, err := os.Stat(dir)
+				if os.IsNotExist(err) {
+					_ = os.MkdirAll(dir, 0777)
+				}
+				_ = e.cfg.Save(localConfigURL)
+			}
+		}
+	}
+	if e.cfg == nil {
+		if localConfigURL == "" {
+			if _, err := os.Stat("conf-file.toml"); err == nil {
+				localConfigURL = "conf-file.toml"
+			}
+		}
+		if localConfigURL != "" {
+			e.cfg = config.NewConfig(localConfigURL)
+		}
+	}
 
-    log.Config(e.cfg.GetString("application.name"),
-        log.Level(e.cfg.GetString("log.level")),
-        e.cfg.GetBool("log.console"),
-        e.cfg.GetString("log.filename"),
-        func() log.LogFormat {
-            if e.cfg.GetString("log.format") == "text" {
-                return log.LogFormatText
-            } else {
-                return log.LogFormatJSON
-            }
-        }(),
-        e.cfg.GetInt("log.maxSize"),
-        e.cfg.GetInt("log.maxBackups"),
-        e.cfg.GetInt("log.maxAge"),
-    )
+	log.Config(e.cfg.GetString("application.name"),
+		log.Level(e.cfg.GetString("log.level")),
+		e.cfg.GetBool("log.console"),
+		e.cfg.GetString("log.filename"),
+		func() log.LogFormat {
+			if e.cfg.GetString("log.format") == "text" {
+				return log.LogFormatText
+			} else {
+				return log.LogFormatJSON
+			}
+		}(),
+		e.cfg.GetInt("log.maxSize"),
+		e.cfg.GetInt("log.maxBackups"),
+		e.cfg.GetInt("log.maxAge"),
+	)
 
-    log.Infof("log level:%s", e.cfg.GetString("log.level"))
-    return nil
+	log.Infof("log level:%s", e.cfg.GetString("log.level"))
+	return nil
 }
 
 func (e *Executor) Start() error {
-    e.s.Main(e.cfg)
-    return nil
+	e.s.Main(e.cfg)
+	return nil
 }
 
 func (e *Executor) Stop() error {
-    if e.s != nil {
-        e.s.Exit()
-    }
-    log.Flush()
-    if e.cfg != nil {
-        e.cfg.Exit()
-    }
-    return nil
+	if e.s != nil {
+		e.s.Exit()
+	}
+	log.Flush()
+	if e.cfg != nil {
+		e.cfg.Exit()
+	}
+	return nil
 }
