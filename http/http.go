2ff532ce6603ca1a36fb2622635f5ede6b8d3ba2
JinlongChen <jinlongchen@live.com>
2024-10-19 10:20:40 +0800

    http -> resty

--- http/http.go
+++ http/http.go
@@ -8,171 +8,75 @@ import (
 	"encoding/xml"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"mime/multipart"
-	netHttp "net/http"
-	"net/http/cookiejar"
 	"net/url"
 	"os"
 	"path"
 	"path/filepath"
-	"strings"
 	"time"
 
+	"github.com/go-resty/resty/v2"
 	"github.com/jinlongchen/golang-utilities/errors"
 )
 
 func GetData(reqURL string) ([]byte, error) {
-	tr := &netHttp.Transport{
-		TLSClientConfig: &tls.Config{},
-	}
-	cookieJar, _ := cookiejar.New(nil)
+	client := resty.New()
+	client.SetTLSClientConfig(&tls.Config{})
+	client.SetTimeout(30 * time.Second)
 
-	client := &netHttp.Client{Transport: tr, Jar: cookieJar, Timeout: time.Duration(time.Second * 30)}
-	request, _ := netHttp.NewRequest("GET", reqURL, nil)
-
-	response, err := client.Do(request)
-
+	resp, err := client.R().Get(reqURL)
 	if err != nil {
-		// log.Errorf( "Get data error:%s", err.Error())
 		return nil, err
 	}
 
-	defer response.Body.Close()
-
-	var body []byte
-
-	switch response.Header.Get("Content-Encoding") {
-	case "gzip":
-		reader, err := gzip.NewReader(response.Body)
-		if err != nil {
-			return nil, err
-		}
-		defer reader.Close()
-
-		body, err = ioutil.ReadAll(reader)
-		if err != nil {
-			return nil, err
-		}
-	default:
-		body, err = ioutil.ReadAll(response.Body)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	if response.StatusCode == 200 {
-		return body, nil
+	if resp.StatusCode() == 200 {
+		return resp.Body(), nil
 	} else {
-		return body, errors.WithCode(nil, fmt.Sprintf("HTTP_%d", response.StatusCode), response.Status)
+		return resp.Body(), errors.WithCode(nil, fmt.Sprintf("HTTP_%d", resp.StatusCode()), resp.Status())
 	}
 }
 
 func GetDataProxy(reqURL string, proxy string) ([]byte, error) {
 	proxyUrl, err := url.Parse(proxy)
-	tr := &netHttp.Transport{
-		TLSClientConfig: &tls.Config{},
-		Proxy:           netHttp.ProxyURL(proxyUrl),
-	}
-	cookieJar, _ := cookiejar.New(nil)
+	client := resty.New()
+	client.SetTLSClientConfig(&tls.Config{})
+	client.SetProxy(proxyUrl.String())
+	client.SetTimeout(30 * time.Second)
 
-	client := &netHttp.Client{Transport: tr, Jar: cookieJar, Timeout: time.Duration(time.Second * 30)}
-	request, _ := netHttp.NewRequest("GET", reqURL, nil)
-
-	response, err := client.Do(request)
-
+	resp, err := client.R().Get(reqURL)
 	if err != nil {
-		// log.Errorf( "Get data error:%s", err.Error())
 		return nil, err
 	}
 
-	defer response.Body.Close()
-
-	var body []byte
-
-	switch response.Header.Get("Content-Encoding") {
-	case "gzip":
-		reader, err := gzip.NewReader(response.Body)
-		if err != nil {
-			return nil, err
-		}
-		defer reader.Close()
-
-		body, err = ioutil.ReadAll(reader)
-		if err != nil {
-			return nil, err
-		}
-	default:
-		body, err = ioutil.ReadAll(response.Body)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	if response.StatusCode == 200 {
-		return body, nil
+	if resp.StatusCode() == 200 {
+		return resp.Body(), nil
 	} else {
-		return body, errors.WithCode(nil, fmt.Sprintf("HTTP_%d", response.StatusCode), response.Status)
+		return resp.Body(), errors.WithCode(nil, fmt.Sprintf("HTTP_%d", resp.StatusCode()), resp.Status())
 	}
 }
+
 func GetDataWithHeaders(reqURL string, reqHeader netHttp.Header) (netHttp.Header, []byte, error) {
-	tr := &netHttp.Transport{
-		TLSClientConfig: &tls.Config{},
-	}
-	cookieJar, _ := cookiejar.New(nil)
+	client := resty.New()
+	client.SetTLSClientConfig(&tls.Config{})
+	client.SetTimeout(30 * time.Second)
 
-	client := &netHttp.Client{Transport: tr, Jar: cookieJar}
-	request, _ := netHttp.NewRequest("GET", reqURL, nil)
-
-	if reqHeader != nil {
-		for key, value := range reqHeader {
-			for _, s := range value {
-				request.Header.Add(key, s)
-			}
-		}
-	}
-
-	response, err := client.Do(request)
+	resp, err := client.R().SetHeaders(reqHeader).Get(reqURL)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	defer response.Body.Close()
-	var body []byte
-	switch response.Header.Get("Content-Encoding") {
-	case "gzip":
-		reader, err := gzip.NewReader(response.Body)
-		if err != nil {
-			return nil, nil, err
-		}
-		defer reader.Close()
-		body, err = ioutil.ReadAll(reader)
-		if err != nil {
-			return nil, nil, err
-		}
-	default:
-		body, err = ioutil.ReadAll(response.Body)
-		if err != nil {
-			return nil, nil, err
-		}
-	}
-
-	if response.StatusCode == 200 {
-		return response.Header, body, nil
+	if resp.StatusCode() == 200 {
+		return resp.Header(), resp.Body(), nil
 	} else {
-		return response.Header, body, errors.WithCode(nil, fmt.Sprintf("HTTP_%d", response.StatusCode), response.Status)
+		return resp.Header(), resp.Body(), errors.WithCode(nil, fmt.Sprintf("HTTP_%d", resp.StatusCode()), resp.Status())
 	}
 }
+
 func GetJSON(url string, out interface{}) error {
-	// resp, err := netHttp.Get(url)
+	client := resty.New()
+	client.SetTimeout(30 * time.Second)
 
-	client := &netHttp.Client{}
-	request, _ := netHttp.NewRequest("GET", url, nil)
-
-	// request.Header.Set("Accept-Encoding", "gzip")
-
-	resp, err := client.Do(request)
-
+	resp, err := client.R().Get(url)
 	if err != nil {
 		return err
 	}
@@ -180,216 +84,114 @@ func GetJSON(url string, out interface{}) error {
 }
 
 func PutDataWithHeaders(reqURL string, reqHeader netHttp.Header, contentType string, data []byte) (netHttp.Header, []byte, error) {
-	tr := &netHttp.Transport{
-		TLSClientConfig: &tls.Config{},
-	}
-	client := &netHttp.Client{
-		Transport: tr,
-		Timeout:   time.Second * 30,
-	}
+	client := resty.New()
+	client.SetTLSClientConfig(&tls.Config{})
+	client.SetTimeout(30 * time.Second)
 
-	request, _ := netHttp.NewRequest("PUT", reqURL, bytes.NewReader(data))
-
-	if reqHeader != nil {
-		for key, value := range reqHeader {
-			for _, s := range value {
-				request.Header.Add(key, s)
-			}
-		}
-	}
-	request.Header.Set("Content-Type", contentType)
-	response, err := client.Do(request)
-
+	resp, err := client.R().
+		SetHeaders(reqHeader).
+		SetHeader("Content-Type", contentType).
+		SetBody(data).
+		Put(reqURL)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	defer response.Body.Close()
-
-	var body []byte
-
-	switch response.Header.Get("Content-Encoding") {
-	case "gzip":
-		reader, err := gzip.NewReader(response.Body)
-		if err != nil {
-			return nil, nil, err
-		}
-		defer reader.Close()
-
-		body, err = ioutil.ReadAll(reader)
-		if err != nil {
-			return nil, nil, err
-		}
-	default:
-		body, err = ioutil.ReadAll(response.Body)
-		if err != nil {
-			return nil, nil, err
-		}
-	}
-	if response.StatusCode == 200 {
-		return response.Header, body, nil
+	if resp.StatusCode() == 200 {
+		return resp.Header(), resp.Body(), nil
 	} else {
-		return response.Header, body, errors.WithCode(nil, fmt.Sprintf("HTTP_%d", response.StatusCode), response.Status)
+		return resp.Header(), resp.Body(), errors.WithCode(nil, fmt.Sprintf("HTTP_%d", resp.StatusCode()), resp.Status())
 	}
 }
 
 func PostData(reqURL string, contentType string, data []byte) ([]byte, error) {
-	timeout := 15 * time.Second
-	client := &netHttp.Client{
-		Timeout: timeout,
-	}
+	client := resty.New()
+	client.SetTimeout(15 * time.Second)
 
-	response, err := client.Post(reqURL, contentType, bytes.NewReader(data))
+	resp, err := client.R().
+		SetHeader("Content-Type", contentType).
+		SetBody(data).
+		Post(reqURL)
 	if err != nil {
 		return nil, err
 	}
 
-	defer response.Body.Close()
-
-	var body []byte
-
-	switch response.Header.Get("Content-Encoding") {
-	case "gzip":
-		reader, err := gzip.NewReader(response.Body)
-		if err != nil {
-			return nil, err
-		}
-		defer reader.Close()
-
-		body, err = ioutil.ReadAll(reader)
-		if err != nil {
-			return nil, err
-		}
-	default:
-		body, err = ioutil.ReadAll(response.Body)
-		if err != nil {
-			return nil, err
-		}
-	}
-	if response.StatusCode == 200 {
-		return body, nil
+	if resp.StatusCode() == 200 {
+		return resp.Body(), nil
 	} else {
-		return body, errors.WithCode(nil, fmt.Sprintf("HTTP_%d", response.StatusCode), response.Status)
+		return resp.Body(), errors.WithCode(nil, fmt.Sprintf("HTTP_%d", resp.StatusCode()), resp.Status())
 	}
 }
+
 func PostDataWithHeaders(reqURL string, reqHeader netHttp.Header, contentType string, data []byte, timeout time.Duration) (netHttp.Header, []byte, error) {
-	tr := &netHttp.Transport{
-		TLSClientConfig: &tls.Config{},
-	}
-	client := &netHttp.Client{
-		Transport: tr,
-		Timeout:   timeout,
-	}
+	client := resty.New()
+	client.SetTLSClientConfig(&tls.Config{})
+	client.SetTimeout(timeout)
 
-	request, _ := netHttp.NewRequest("POST", reqURL, bytes.NewReader(data))
-
-	if reqHeader != nil {
-		for key, value := range reqHeader {
-			for _, s := range value {
-				request.Header.Add(key, s)
-			}
-		}
-	}
-	request.Header.Set("Content-Type", contentType)
-	response, err := client.Do(request)
-
+	resp, err := client.R().
+		SetHeaders(reqHeader).
+		SetHeader("Content-Type", contentType).
+		SetBody(data).
+		Post(reqURL)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	defer response.Body.Close()
-
-	var body []byte
-
-	switch response.Header.Get("Content-Encoding") {
-	case "gzip":
-		reader, err := gzip.NewReader(response.Body)
-		if err != nil {
-			return nil, nil, err
-		}
-		defer reader.Close()
-
-		body, err = ioutil.ReadAll(reader)
-		if err != nil {
-			return nil, nil, err
-		}
-	default:
-		body, err = ioutil.ReadAll(response.Body)
-		if err != nil {
-			return nil, nil, err
-		}
-	}
-	if response.StatusCode == 200 {
-		return response.Header, body, nil
+	if resp.StatusCode() == 200 {
+		return resp.Header(), resp.Body(), nil
 	} else {
-		return response.Header, body, errors.WithCode(nil, fmt.Sprintf("HTTP_%d", response.StatusCode), response.Status)
+		return resp.Header(), resp.Body(), errors.WithCode(nil, fmt.Sprintf("HTTP_%d", resp.StatusCode()), resp.Status())
 	}
 }
-func PostJSON(reqURL string, objToSend interface{}, out interface{}) error {
-	jsonData, err := json.Marshal(objToSend)
-	if err != nil {
-		// log.Errorf( "marshal json err:%s", err.Error())
-		return err
-	}
 
-	resp, err := netHttp.Post(reqURL, "application/json;charset=utf-8", bytes.NewReader(jsonData))
+func PostJSON(reqURL string, objToSend interface{}, out interface{}) error {
+	client := resty.New()
+	client.SetTimeout(30 * time.Second)
+
+	resp, err := client.R().
+		SetHeader("Content-Type", "application/json;charset=utf-8").
+		SetBody(objToSend).
+		Post(reqURL)
 	if err != nil {
-		// log.Errorf( "post json data to(%s)  err:%s", reqURL, err.Error())
 		return err
 	}
 	return readJSON(resp, out)
 }
+
 func PostDataSsl(reqURL string, dataToSend, certPEMBlock, keyPEMBlock []byte) (respData []byte, err error) {
 	cert, err := tls.X509KeyPair(certPEMBlock, keyPEMBlock)
 	if err != nil {
 		return nil, err
 	}
 
-	tr := &netHttp.Transport{
-		TLSClientConfig: &tls.Config{Certificates: []tls.Certificate{cert}, InsecureSkipVerify: true},
-	}
-	client := &netHttp.Client{Transport: tr}
+	client := resty.New()
+	client.SetTLSClientConfig(&tls.Config{Certificates: []tls.Certificate{cert}, InsecureSkipVerify: true})
 
-	ret, err := client.Post(reqURL, "application/x-www-form-urlencoded", bytes.NewReader(dataToSend))
-	if err != nil {
-		return nil, err
-	}
-	defer func() {
-		err = ret.Body.Close()
-		if err != nil {
-			// log.Errorf( err.Error())
-		}
-	}()
-
-	data, err := ioutil.ReadAll(ret.Body)
-
+	resp, err := client.R().
+		SetHeader("Content-Type", "application/x-www-form-urlencoded").
+		SetBody(dataToSend).
+		Post(reqURL)
 	if err != nil {
 		return nil, err
 	}
 
-	return data, err
+	return resp.Body(), nil
 }
 
 func PostXml(reqURL string, xmlToSend string, objReceived interface{}) (respData []byte, err error) {
-	ret, err := netHttp.Post(reqURL, "application/x-www-form-urlencoded;charset=utf-8", strings.NewReader(xmlToSend))
+	client := resty.New()
+	client.SetTimeout(30 * time.Second)
 
+	resp, err := client.R().
+		SetHeader("Content-Type", "application/x-www-form-urlencoded;charset=utf-8").
+		SetBody(xmlToSend).
+		Post(reqURL)
 	if err != nil {
-		// log.Errorf( "post xml err:%s", err.Error())
 		return nil, err
 	}
 
-	defer ret.Body.Close()
-
-	data, err := ioutil.ReadAll(ret.Body)
-
-	if err != nil {
-		// log.Errorf( "post xml err:%s", err.Error())
-		return nil, err
-	}
-
-	err = xml.Unmarshal(data, objReceived)
-
-	return data, err
+	err = xml.Unmarshal(resp.Body(), objReceived)
+	return resp.Body(), err
 }
 
 func PostFiles(reqURL string, filesVal map[string][]string, form map[string]string, progressReporter func(r int64)) (ret []byte, err error) {
@@ -426,72 +228,41 @@ func PostFiles(reqURL string, filesVal map[string][]string, form map[string]stri
 
 	w.Close()
 
-	client := &netHttp.Client{}
-	request, _ := netHttp.NewRequest("POST", reqURL, &b)
+	client := resty.New()
+	client.SetTimeout(30 * time.Second)
 
-	request.Header.Set("Content-Type", w.FormDataContentType())
-
-	response, err := client.Do(request)
+	resp, err := client.R().
+		SetHeader("Content-Type", w.FormDataContentType()).
+		SetBody(&b).
+		Post(reqURL)
 	if err != nil {
 		return
 	}
 
-	defer response.Body.Close()
-
-	var body []byte
-
-	switch response.Header.Get("Content-Encoding") {
-	case "gzip":
-		var reader *gzip.Reader
-		reader, err = gzip.NewReader(response.Body)
-		if err != nil {
-			return
-		}
-		defer reader.Close()
-
-		body, err = ioutil.ReadAll(reader)
-		if err != nil {
-			return
-		}
-	default:
-		body, err = ioutil.ReadAll(response.Body)
-		if err != nil {
-			return
-		}
-	}
-
-	if response.StatusCode == 200 {
-		return body, nil
+	if resp.StatusCode() == 200 {
+		return resp.Body(), nil
 	} else {
-		return body, errors.WithCode(nil, fmt.Sprintf("HTTP_%d", response.StatusCode), response.Status)
+		return resp.Body(), errors.WithCode(nil, fmt.Sprintf("HTTP_%d", resp.StatusCode()), resp.Status())
 	}
 }
+
 func DownloadFile(reqURL string, filePath string) error {
-	tr := &netHttp.Transport{
-		// TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-	cookieJar, _ := cookiejar.New(nil)
+	client := resty.New()
+	client.SetTimeout(30 * time.Second)
 
-	client := &netHttp.Client{Transport: tr, Jar: cookieJar, Timeout: time.Duration(time.Second * 30)}
-	request, _ := netHttp.NewRequest("GET", reqURL, nil)
-
-	response, err := client.Do(request)
-
+	resp, err := client.R().Get(reqURL)
 	if err != nil {
-		// log.Errorf( "Get data error:%s", err.Error())
 		return err
 	}
-
-	defer response.Body.Close()
 
 	dir := path.Dir(filePath)
 	if err := os.MkdirAll(dir, 0755); err != nil {
 		return err
 	}
 
-	switch response.Header.Get("Content-Encoding") {
+	switch resp.Header().Get("Content-Encoding") {
 	case "gzip":
-		reader, err := gzip.NewReader(response.Body)
+		reader, err := gzip.NewReader(bytes.NewReader(resp.Body()))
 		if err != nil {
 			return err
 		}
@@ -514,92 +285,56 @@ func DownloadFile(reqURL string, filePath string) error {
 		}
 		defer writer.Close()
 
-		_, err = io.Copy(writer, response.Body)
+		_, err = io.Copy(writer, bytes.NewReader(resp.Body()))
 		if err != nil {
 			return err
 		}
 	}
-	if response.StatusCode == 200 {
+	if resp.StatusCode() == 200 {
 		return nil
 	} else {
-		return errors.WithCode(nil, fmt.Sprintf("HTTP_%d", response.StatusCode), response.Status)
+		return errors.WithCode(nil, fmt.Sprintf("HTTP_%d", resp.StatusCode()), resp.Status())
 	}
 }
 
 func DeleteDataWithHeaders(reqURL string, reqHeader netHttp.Header) (netHttp.Header, []byte, error) {
-	tr := &netHttp.Transport{
-		TLSClientConfig: &tls.Config{},
-	}
-	cookieJar, _ := cookiejar.New(nil)
+	client := resty.New()
+	client.SetTLSClientConfig(&tls.Config{})
+	client.SetTimeout(30 * time.Second)
 
-	client := &netHttp.Client{Transport: tr, Jar: cookieJar}
-	request, _ := netHttp.NewRequest("DELETE", reqURL, nil)
-
-	if reqHeader != nil {
-		for key, value := range reqHeader {
-			for _, s := range value {
-				request.Header.Add(key, s)
-			}
-		}
-	}
-
-	response, err := client.Do(request)
+	resp, err := client.R().SetHeaders(reqHeader).Delete(reqURL)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	defer response.Body.Close()
-	var body []byte
-	switch response.Header.Get("Content-Encoding") {
-	case "gzip":
-		reader, err := gzip.NewReader(response.Body)
-		if err != nil {
-			return nil, nil, err
-		}
-		defer reader.Close()
-		body, err = ioutil.ReadAll(reader)
-		if err != nil {
-			return nil, nil, err
-		}
-	default:
-		body, err = ioutil.ReadAll(response.Body)
-		if err != nil {
-			return nil, nil, err
-		}
-	}
-
-	if response.StatusCode == 200 {
-		return response.Header, body, nil
+	if resp.StatusCode() == 200 {
+		return resp.Header(), resp.Body(), nil
 	} else {
-		return response.Header, body, errors.WithCode(nil, fmt.Sprintf("HTTP_%d", response.StatusCode), response.Status)
+		return resp.Header(), resp.Body(), errors.WithCode(nil, fmt.Sprintf("HTTP_%d", resp.StatusCode()), resp.Status())
 	}
 }
 
-func readJSON(resp *netHttp.Response, out interface{}) (err error) {
-	defer resp.Body.Close()
-
+func readJSON(resp *resty.Response, out interface{}) (err error) {
 	var reader io.ReadCloser
-	// // log.Infof( "Content-Encoding:%s", resp.Header.Get("Content-Encoding"))
-	switch resp.Header.Get("Content-Encoding") {
+	switch resp.Header().Get("Content-Encoding") {
 	case "gzip":
-		// log.Infof( "response:gzip")
-		reader, err = gzip.NewReader(resp.Body)
+		reader, err = gzip.NewReader(bytes.NewReader(resp.Body()))
 		defer reader.Close()
 	default:
-		reader = resp.Body
+		reader = io.NopCloser(bytes.NewReader(resp.Body()))
 	}
 
-	if resp.StatusCode >= 400 {
-		body, err := ioutil.ReadAll(reader)
+	if resp.StatusCode() >= 400 {
+		body, err := io.ReadAll(reader)
 		if err != nil {
 			return err
 		}
 
-		return errors.WithCode(nil, fmt.Sprintf("HTTP_%d", resp.StatusCode), string(body))
+		return errors.WithCode(nil, fmt.Sprintf("HTTP_%d", resp.StatusCode()), string(body))
 	}
 
 	if out == nil {
-		io.Copy(ioutil.Discard, reader)
+		io.Copy(io.Discard, reader)
 		return nil
 	}
 
--- http/util.go
+++ http/util.go
@@ -1,209 +1,209 @@
 package http
 
 import (
-    "io/ioutil"
-    "net/http"
-    "net/url"
-    "path"
-    "path/filepath"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"path"
+	"path/filepath"
 
-    "github.com/jinlongchen/golang-utilities/json"
+	"github.com/jinlongchen/golang-utilities/json"
 )
 
 func GetRequestBody(r *http.Request) []byte {
-    if r.Method == "POST" || r.Method == "PUT" {
-        data, err := ioutil.ReadAll(r.Body)
-        if err != nil {
-            return nil
-        }
-        return data
-    } else {
-        return nil
-    }
+	if r.Method == "POST" || r.Method == "PUT" {
+		data, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			return nil
+		}
+		return data
+	} else {
+		return nil
+	}
 }
 
 func GetRequestBodyAsMap(r *http.Request) map[string]interface{} {
-    data, err := ioutil.ReadAll(r.Body)
-    if err != nil {
-        return nil
-    }
-    ret := make(map[string]interface{})
-    err = json.Unmarshal(data, &ret)
-    if err != nil {
-        return nil
-    }
-    return ret
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return nil
+	}
+	ret := make(map[string]interface{})
+	err = json.Unmarshal(data, &ret)
+	if err != nil {
+		return nil
+	}
+	return ret
 }
 
 func GetRequestObject[T any](r *http.Request) (T, error) {
-    var t T
-    data, err := ioutil.ReadAll(r.Body)
-    if err != nil {
-        return t, err
-    }
-    err = json.Unmarshal(data, &t)
-    if err != nil {
-        return t, err
-    }
-    return t, nil
+	var t T
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return t, err
+	}
+	err = json.Unmarshal(data, &t)
+	if err != nil {
+		return t, err
+	}
+	return t, nil
 }
 
 func AddURLQuery(reqURL string, key, value string) string {
-    x, err := url.Parse(reqURL)
-    if err != nil {
-        return reqURL
-    }
-    q := x.Query()
-    q.Set(key, value)
-    x.RawQuery = q.Encode()
-    return x.String()
+	x, err := url.Parse(reqURL)
+	if err != nil {
+		return reqURL
+	}
+	q := x.Query()
+	q.Set(key, value)
+	x.RawQuery = q.Encode()
+	return x.String()
 }
 
 func ContentTypeByUrl(httpUrl string) string {
-    ext := ""
-    u, err := url.Parse(httpUrl)
-    if err == nil {
-        ext = filepath.Ext(path.Base(u.Path))
-    }
-    switch ext {
-    case ".jpg":
-        return `image/jpeg`
-    case ".png":
-        return `image/png`
-    case ".gif":
-        return `image/gif`
-    case ".webp":
-        return `image/webp`
-    case ".cr2":
-        return `image/x-canon-cr2`
-    case ".tif":
-        return `image/tiff`
-    case ".bmp":
-        return `image/bmp`
-    case ".jxr":
-        return `image/vnd.ms-photo`
-    case ".psd":
-        return `image/vnd.adobe.photoshop`
-    case ".ico":
-        return `image/x-icon`
-    case ".mp4":
-        return `video/mp4`
-    case ".m4v":
-        return `video/x-m4v`
-    case ".mkv":
-        return `video/x-matroska`
-    case ".webm":
-        return `video/webm`
-    case ".mov":
-        return `video/quicktime`
-    case ".avi":
-        return `video/x-msvideo`
-    case ".wmv":
-        return `video/x-ms-wmv`
-    case ".mpg":
-        return `video/mpeg`
-    case ".flv":
-        return `video/x-flv`
-    case ".mid":
-        return `audio/midi`
-    case ".mp3":
-        return `audio/mpeg`
-    case ".m4a":
-        return `audio/m4a`
-    case ".ogg":
-        return `audio/ogg`
-    case ".flac":
-        return `audio/x-flac`
-    case ".wav":
-        return `audio/x-wav`
-    case ".amr":
-        return `audio/amr`
-    case ".epub":
-        return `application/epub+zip`
-    case ".zip":
-        return `application/zip`
-    case ".tar":
-        return `application/x-tar`
-    case ".rar":
-        return `application/x-rar-compressed`
-    case ".gz":
-        return `application/gzip`
-    case ".bz2":
-        return `application/x-bzip2`
-    case ".7z":
-        return `application/x-7z-compressed`
-    case ".xz":
-        return `application/x-xz`
-    case ".pdf":
-        return `application/pdf`
-    case ".exe":
-        return `application/x-msdownload`
-    case ".swf":
-        return `application/x-shockwave-flash`
-    case ".rtf":
-        return `application/rtf`
-    case ".eot":
-        return `application/octet-stream`
-    case ".ps":
-        return `application/postscript`
-    case ".sqlite":
-        return `application/x-sqlite3`
-    case ".nes":
-        return `application/x-nintendo-nes-rom`
-    case ".crx":
-        return `application/x-google-chrome-extension`
-    case ".cab":
-        return `application/vnd.ms-cab-compressed`
-    case ".deb":
-        return `application/x-deb`
-    case ".ar":
-        return `application/x-unix-archive`
-    case ".Z":
-        return `application/x-compress`
-    case ".lz":
-        return `application/x-lzip`
-    case ".rpm":
-        return `application/x-rpm`
-    case ".elf":
-        return `application/x-executable`
-    case ".doc":
-        return `application/msword`
-    case ".docx":
-        return `application/vnd.openxmlformats-officedocument.wordprocessingml.document`
-    case ".xls":
-        return `application/vnd.ms-excel`
-    case ".xlsx":
-        return `application/vnd.openxmlformats-officedocument.spreadsheetml.sheet`
-    case ".ppt":
-        return `application/vnd.ms-powerpoint`
-    case ".pptx":
-        return `application/vnd.openxmlformats-officedocument.presentationml.presentation`
-    case ".woff":
-        return `application/font-woff`
-    case ".woff2":
-        return `application/font-woff`
-    case ".ttf":
-        return `application/font-sfnt`
-    case ".otf":
-        return `application/font-sfnt`
-    default:
-        return "application/octet-stream"
-    }
+	ext := ""
+	u, err := url.Parse(httpUrl)
+	if err == nil {
+		ext = filepath.Ext(path.Base(u.Path))
+	}
+	switch ext {
+	case ".jpg":
+		return `image/jpeg`
+	case ".png":
+		return `image/png`
+	case ".gif":
+		return `image/gif`
+	case ".webp":
+		return `image/webp`
+	case ".cr2":
+		return `image/x-canon-cr2`
+	case ".tif":
+		return `image/tiff`
+	case ".bmp":
+		return `image/bmp`
+	case ".jxr":
+		return `image/vnd.ms-photo`
+	case ".psd":
+		return `image/vnd.adobe.photoshop`
+	case ".ico":
+		return `image/x-icon`
+	case ".mp4":
+		return `video/mp4`
+	case ".m4v":
+		return `video/x-m4v`
+	case ".mkv":
+		return `video/x-matroska`
+	case ".webm":
+		return `video/webm`
+	case ".mov":
+		return `video/quicktime`
+	case ".avi":
+		return `video/x-msvideo`
+	case ".wmv":
+		return `video/x-ms-wmv`
+	case ".mpg":
+		return `video/mpeg`
+	case ".flv":
+		return `video/x-flv`
+	case ".mid":
+		return `audio/midi`
+	case ".mp3":
+		return `audio/mpeg`
+	case ".m4a":
+		return `audio/m4a`
+	case ".ogg":
+		return `audio/ogg`
+	case ".flac":
+		return `audio/x-flac`
+	case ".wav":
+		return `audio/x-wav`
+	case ".amr":
+		return `audio/amr`
+	case ".epub":
+		return `application/epub+zip`
+	case ".zip":
+		return `application/zip`
+	case ".tar":
+		return `application/x-tar`
+	case ".rar":
+		return `application/x-rar-compressed`
+	case ".gz":
+		return `application/gzip`
+	case ".bz2":
+		return `application/x-bzip2`
+	case ".7z":
+		return `application/x-7z-compressed`
+	case ".xz":
+		return `application/x-xz`
+	case ".pdf":
+		return `application/pdf`
+	case ".exe":
+		return `application/x-msdownload`
+	case ".swf":
+		return `application/x-shockwave-flash`
+	case ".rtf":
+		return `application/rtf`
+	case ".eot":
+		return `application/octet-stream`
+	case ".ps":
+		return `application/postscript`
+	case ".sqlite":
+		return `application/x-sqlite3`
+	case ".nes":
+		return `application/x-nintendo-nes-rom`
+	case ".crx":
+		return `application/x-google-chrome-extension`
+	case ".cab":
+		return `application/vnd.ms-cab-compressed`
+	case ".deb":
+		return `application/x-deb`
+	case ".ar":
+		return `application/x-unix-archive`
+	case ".Z":
+		return `application/x-compress`
+	case ".lz":
+		return `application/x-lzip`
+	case ".rpm":
+		return `application/x-rpm`
+	case ".elf":
+		return `application/x-executable`
+	case ".doc":
+		return `application/msword`
+	case ".docx":
+		return `application/vnd.openxmlformats-officedocument.wordprocessingml.document`
+	case ".xls":
+		return `application/vnd.ms-excel`
+	case ".xlsx":
+		return `application/vnd.openxmlformats-officedocument.spreadsheetml.sheet`
+	case ".ppt":
+		return `application/vnd.ms-powerpoint`
+	case ".pptx":
+		return `application/vnd.openxmlformats-officedocument.presentationml.presentation`
+	case ".woff":
+		return `application/font-woff`
+	case ".woff2":
+		return `application/font-woff`
+	case ".ttf":
+		return `application/font-sfnt`
+	case ".otf":
+		return `application/font-sfnt`
+	default:
+		return "application/octet-stream"
+	}
 }
 
 func GetRemoteIP(req *http.Request) string {
-    if req.Header.Get("X-Forwarded-For") != "" {
-        return req.Header.Get("X-Forwarded-For")
-    }
-    if req.Header.Get("X-Real-IP") != "" {
-        return req.Header.Get("X-Real-IP")
-    }
-    if req.Header.Get("Proxy-Client-IP") != "" {
-        return req.Header.Get("Proxy-Client-IP")
-    }
-    if req.Header.Get("WL-Proxy-Client-IP") != "" {
-        return req.Header.Get("WL-Proxy-Client-IP")
-    }
+	if req.Header.Get("X-Forwarded-For") != "" {
+		return req.Header.Get("X-Forwarded-For")
+	}
+	if req.Header.Get("X-Real-IP") != "" {
+		return req.Header.Get("X-Real-IP")
+	}
+	if req.Header.Get("Proxy-Client-IP") != "" {
+		return req.Header.Get("Proxy-Client-IP")
+	}
+	if req.Header.Get("WL-Proxy-Client-IP") != "" {
+		return req.Header.Get("WL-Proxy-Client-IP")
+	}
 
-    return req.RemoteAddr
+	return req.RemoteAddr
 }
