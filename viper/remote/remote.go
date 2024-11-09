853521fc8d8d31d600923d10b2377b4370b4fa4a
JinlongChen <jinlongchen@live.com>
2024-11-09 20:39:11 +0800

    viper(remote)

--- viper/remote/remote.go
+++ viper/remote/remote.go
@@ -7,99 +7,102 @@
 package remote
 
 import (
-    "bytes"
-    "io"
-    "os"
+	"bytes"
+	"io"
+	"os"
 
-    "github.com/jinlongchen/golang-utilities/viper"
-    crypt "github.com/jinlongchen/golang-utilities/viper/crypt/config"
+	"github.com/jinlongchen/golang-utilities/viper"
+	crypt "github.com/jinlongchen/golang-utilities/viper/crypt/config"
 )
 
 type remoteConfigProvider struct{}
 
 func (rc remoteConfigProvider) Get(rp viper.RemoteProvider) (io.Reader, error) {
-    cm, err := getConfigManager(rp)
-    if err != nil {
-        return nil, err
-    }
-    b, err := cm.Get(rp.Path())
-    if err != nil {
-        return nil, err
-    }
-    return bytes.NewReader(b), nil
+	return rc.fetchConfig(rp)
 }
 
 func (rc remoteConfigProvider) Watch(rp viper.RemoteProvider) (io.Reader, error) {
-    cm, err := getConfigManager(rp)
-    if err != nil {
-        return nil, err
-    }
-    resp, err := cm.Get(rp.Path())
-    if err != nil {
-        return nil, err
-    }
+	return rc.fetchConfig(rp)
+}
 
-    return bytes.NewReader(resp), nil
+func (rc remoteConfigProvider) fetchConfig(rp viper.RemoteProvider) (io.Reader, error) {
+	cm, err := getConfigManager(rp)
+	if err != nil {
+		return nil, err
+	}
+	b, err := cm.Get(rp.Path())
+	if err != nil {
+		return nil, err
+	}
+	return bytes.NewReader(b), nil
 }
 
 func (rc remoteConfigProvider) WatchChannel(rp viper.RemoteProvider) (<-chan *viper.RemoteResponse, chan bool) {
-    cm, err := getConfigManager(rp)
-    if err != nil {
-        return nil, nil
-    }
-    quit := make(chan bool)
-    quitwc := make(chan bool)
-    viperResponsCh := make(chan *viper.RemoteResponse)
-    cryptoResponseCh := cm.Watch(rp.Path(), quit)
-    // need this function to convert the Channel response form crypt.Response to viper.Response
-    go func(cr <-chan *crypt.Response, vr chan<- *viper.RemoteResponse, quitwc <-chan bool, quit chan<- bool) {
-        for {
-            select {
-            case <-quitwc:
-                quit <- true
-                return
-            case resp := <-cr:
-                vr <- &viper.RemoteResponse{
-                    Error: resp.Error,
-                    Value: resp.Value,
-                }
+	cm, err := getConfigManager(rp)
+	if err != nil {
+		return nil, nil
+	}
+	quit := make(chan bool)
+	quitwc := make(chan bool)
+	viperResponsCh := make(chan *viper.RemoteResponse)
+	cryptoResponseCh := cm.Watch(rp.Path(), quit)
 
-            }
+	go rc.convertResponse(cryptoResponseCh, viperResponsCh, quitwc, quit)
 
-        }
-    }(cryptoResponseCh, viperResponsCh, quitwc, quit)
+	return viperResponsCh, quitwc
+}
 
-    return viperResponsCh, quitwc
+func (rc remoteConfigProvider) convertResponse(cr <-chan *crypt.Response, vr chan<- *viper.RemoteResponse, quitwc <-chan bool, quit chan<- bool) {
+	for {
+		select {
+		case <-quitwc:
+			quit <- true
+			return
+		case resp := <-cr:
+			vr <- &viper.RemoteResponse{
+				Error: resp.Error,
+				Value: resp.Value,
+			}
+		}
+	}
 }
 
 func getConfigManager(rp viper.RemoteProvider) (crypt.ConfigManager, error) {
-    var cm crypt.ConfigManager
-    var err error
+	var cm crypt.ConfigManager
+	var err error
 
-    if rp.SecretKeyring() != "" {
-        kr, err := os.Open(rp.SecretKeyring())
-        defer kr.Close()
-        if err != nil {
-            return nil, err
-        }
-        if rp.Provider() == "etcd" {
-            cm, err = crypt.NewEtcdConfigManager([]string{rp.Endpoint()}, kr)
-        } else {
-            cm, err = crypt.NewConsulConfigManager([]string{rp.Endpoint()}, kr)
-        }
-    } else {
-        if rp.Provider() == "etcd" {
-            cm, err = crypt.NewStandardEtcdConfigManager([]string{rp.Endpoint()})
-        } else {
-            cm, err = crypt.NewStandardConsulConfigManager([]string{rp.Endpoint()})
-        }
-    }
-    if err != nil {
-        return nil, err
-    }
-    return cm, nil
+	if rp.SecretKeyring() != "" {
+		kr, err := os.Open(rp.SecretKeyring())
+		if err != nil {
+			return nil, err
+		}
+		defer kr.Close()
+
+		cm, err = newConfigManagerWithKeyring(rp, kr)
+	} else {
+		cm, err = newStandardConfigManager(rp)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+	return cm, nil
+}
+
+func newConfigManagerWithKeyring(rp viper.RemoteProvider, kr *os.File) (crypt.ConfigManager, error) {
+	if rp.Provider() == "etcd" {
+		return crypt.NewEtcdConfigManager([]string{rp.Endpoint()}, kr)
+	}
+	return crypt.NewConsulConfigManager([]string{rp.Endpoint()}, kr)
+}
+
+func newStandardConfigManager(rp viper.RemoteProvider) (crypt.ConfigManager, error) {
+	if rp.Provider() == "etcd" {
+		return crypt.NewStandardEtcdConfigManager([]string{rp.Endpoint()})
+	}
+	return crypt.NewStandardConsulConfigManager([]string{rp.Endpoint()})
 }
 
 func init() {
-    viper.RemoteConfig = &remoteConfigProvider{}
+	viper.RemoteConfig = &remoteConfigProvider{}
 }
