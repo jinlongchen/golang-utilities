44229b6e85afd6ba7f74bec971f62c88cb7e13be
JinlongChen <jinlongchen@live.com>
2024-12-18 22:08:27 +0800

    id card

--- idcard/idcardgen/idcard.go
+++ idcard/idcardgen/idcard.go
@@ -1,65 +1,71 @@
 package main
 
 import (
-    "encoding/json"
-    "time"
+	"encoding/json"
+	"net/http"
+	"time"
 
-    "github.com/jinlongchen/golang-utilities/idcard"
-    "github.com/jinlongchen/golang-utilities/rand"
+	"github.com/jinlongchen/golang-utilities/idcard"
+	"github.com/jinlongchen/golang-utilities/rand"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 type Street struct {
-    Code         string `json:"code" xml:"code"`
-    Name         string `json:"name" xml:"name"`
-    AreaCode     string `json:"areaCode" xml:"areaCode"`
-    CityCode     string `json:"cityCode" xml:"cityCode"`
-    ProvinceCode string `json:"provinceCode" xml:"provinceCode"`
+	Code         string `json:"code" xml:"code"`
+	Name         string `json:"name" xml:"name"`
+	AreaCode     string `json:"areaCode" xml:"areaCode"`
+	CityCode     string `json:"cityCode" xml:"cityCode"`
+	ProvinceCode string `json:"provinceCode" xml:"provinceCode"`
 }
 type Province struct {
-    Code string `json:"code" xml:"code"`
-    Name string `json:"name" xml:"name"`
+	Code string `json:"code" xml:"code"`
+	Name string `json:"name" xml:"name"`
+}
+
+func generateIDCard(c echo.Context) error {
+	streets := make([]*Street, 0)
+	err := json.Unmarshal([]byte(idcard.StreetsJson), &streets)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, "Error unmarshalling streets")
+	}
+	provinces := make([]*Province, 0)
+	err = json.Unmarshal([]byte(idcard.ProvincesJson), &provinces)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, "Error unmarshalling provinces")
+	}
+
+	areaIndex := rand.GetRandInt(0, len(streets))
+	t := time.Now()
+	sexStr := "M"
+	if rand.GetRandInt(0, 2) == 1 {
+		sexStr = "F"
+	}
+	number := idcard.GenResidentIdCard(streets[areaIndex].AreaCode, t, sexStr)
+	provinceName := ""
+	for _, province := range provinces {
+		if province.Code == streets[areaIndex].ProvinceCode {
+			provinceName = province.Name
+		}
+	}
+	valid, _, _ := idcard.IsResidentIdCard(number)
+	if !valid {
+		return c.JSON(http.StatusInternalServerError, "Generated invalid ID: "+number)
+	}
+	return c.JSON(http.StatusOK, map[string]string{
+		"id":        number,
+		"birthdate": t.Format("2006-01-02"),
+		"sex":       sexStr,
+		"province":  provinceName,
+	})
 }
 
 func main() {
-    streets := make([]*Street, 0)
-    err := json.Unmarshal([]byte(idcard.StreetsJson), &streets)
-    if err != nil {
-        return
-    }
-    provinces := make([]*Province, 0)
-    err = json.Unmarshal([]byte(idcard.ProvincesJson), &provinces)
-    if err != nil {
-        return
-    }
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
-    for offset := 0; offset < 100; offset++ {
-        areaIndex := rand.GetRandInt(0, len(streets))
+	e.GET("/generate-id", generateIDCard)
 
-        var t time.Time
-
-        if offset == 0 {
-            t = time.Now()
-        } else {
-            t = time.Now().Add(-time.Hour*24*time.Duration(rand.GetRandInt(10, 365))).AddDate(-offset, 0, 0)
-        }
-        var sexStr string
-        if rand.GetRandInt(0, 2) == 1 {
-            sexStr = "M"
-        } else {
-            sexStr = "F"
-        }
-        number := idcard.GenResidentIdCard(streets[areaIndex].AreaCode, t, sexStr)
-        provinceName := ""
-        for _, province := range provinces {
-            if province.Code == streets[areaIndex].ProvinceCode {
-                provinceName = province.Name
-            }
-        }
-        valid, _, _ := idcard.IsResidentIdCard(number)
-        if !valid {
-            panic("invalid id:" + number)
-        }
-        println(number, t.Format("2006-01-02"), sexStr, provinceName)
-    }
-
+	e.Logger.Fatal(e.Start(":8080"))
 }
